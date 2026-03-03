288c19d6a5c71aac7abfdc719d0328b239a18448
seethala e v <sheethalev@gmail.com>
2026-03-03 22:35:10 +0530

    updating cofig file for adding screnshot

--- playwright.config.js
+++ playwright.config.js
@@ -31,6 +31,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
+    browserName:"chromium",
     
     launchOptions:{
       slowMo:300,
